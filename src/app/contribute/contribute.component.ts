5d4ebc0b84f20c839e4ca65e3ba6f33382a7e322
Gustav Svensson <gsvensso@tibco.com>
2019-08-07 19:44:20 +0200

    Connection setting

--- src/app/contribute/contribute.component.ts
+++ src/app/contribute/contribute.component.ts
@@ -14,10 +14,14 @@ export class ContributeComponent implements OnInit {
   private id: string;
   public SelectedFile: File;
   private AllowedExtensions;
+  private error: Boolean;
+  private uploadFinished: Boolean;
 
   constructor(private socket: Socket) { }
 
   ngOnInit() {
+    this.error = false;
+    this.uploadFinished = false;
     this.AllowedExtensions = ['jpg', 'jpeg', 'gif', 'png', 'mp4', 'avi', 'flv'];
     this.socket.fromEvent<any>('moreData').subscribe(data => {
       this.UpdateBar(data['Percent']);
@@ -29,6 +33,8 @@ export class ContributeComponent implements OnInit {
       this.FReader.readAsBinaryString(NewFile);
     });
     this.socket.fromEvent<any>('done').subscribe(() => {
+      this.error = false;
+      this.uploadFinished = true;
       this.UpdateBar(100);
       document.getElementById('back').style.display = 'block';
       document.getElementById('thankyou').style.display = 'block';
@@ -52,9 +58,31 @@ export class ContributeComponent implements OnInit {
       }
     });
 
+    // Remove submit button, progress bar and show error on disconnect
+    this.socket.fromEvent<any>('disconnect').subscribe(() => {
+      this.error = true;
+      document.getElementById('connection_status').style.display = 'block';
+      // Only show the error on the progress page and hide the progress bar if it isn't finished
+      if (!this.uploadFinished) {
+        document.getElementById('error').style.display = 'block';
+        document.getElementById('progressBar').style.display = 'none';
+
+      }
+      // Warning is the "please dont close this window" warning
+      document.getElementById('warning').style.display = 'none';
+
+      document.getElementById('submit').style.display = 'none';
+      document.getElementById('back').style.display = 'block';
+    });
+    this.socket.fromEvent<any>('connect').subscribe(() => {
+      document.getElementById('connection_status').style.display = 'none';
+      document.getElementById('submit').style.display = 'block';
+    });
+
     this.ready();
   }
 
+  // Show error about file
   showFileError(error) {
     document.getElementById('fileError').innerHTML = error;
   }
@@ -62,6 +90,7 @@ export class ContributeComponent implements OnInit {
     document.getElementById('fileError').innerHTML = "";
   }
 
+  // initiate eventlisteners
   ready() { 
     document.getElementById('close').addEventListener('click', this.Close);
     document.getElementById('submit').addEventListener('click', this.StartUpload);  
@@ -75,13 +104,19 @@ export class ContributeComponent implements OnInit {
     }
   }
 
-
+  // Function that is being run on submit event, will try to start the upload
   StartUpload = () => {
     let message   = (<HTMLInputElement>document.getElementById('message')).value;
     let twitter   = (<HTMLInputElement>document.getElementById('twitter')).value;
     let instagram = (<HTMLInputElement>document.getElementById('instagram')).value;
     let tumblr    = (<HTMLInputElement>document.getElementById('tumblr')).value;
+    this.uploadFinished = false;
+    this.error = true;
+    // Clear errors
     this.clearFileError();
+    document.getElementById('error').style.display = 'none';
+    // Warning is the "please dont close this window" warning
+    document.getElementById('warning').style.display = 'block';
 
     // Check if the form input is valid
     if (!document.forms['submitform'].reportValidity()) {
@@ -118,26 +153,45 @@ export class ContributeComponent implements OnInit {
     }
   }
 
+  // Function that is being run on file change event
   FileChosen = (event) => {
     this.SelectedFile = event.target.files[0];
   }
 
+  // Update the progress bar
   UpdateBar(percent) {
     // Update progressbar
     document.getElementById('innerProgressBar').style.width = percent + '%';
     document.getElementById('percent').innerHTML = (Math.round(percent*100)/100) + '%';
   }
 
+  // Reset input values
+  resetInput() {
+    (<HTMLInputElement>document.getElementById('message')).value = "";
+    (<HTMLInputElement>document.getElementById('twitter')).value = "";
+    (<HTMLInputElement>document.getElementById('instagram')).value = "";
+    (<HTMLInputElement>document.getElementById('tumblr')).value = "";
+    (<HTMLInputElement>document.getElementById('image')).value = "";
+  }
+
+  // Input values will only be cleared if no error has occured
   Close = () => {
     this.UpdateBar(0);
     document.getElementById('submitform').style.display = 'inline-grid';
     document.getElementById('progress').style.display = 'none';
     document.getElementById('containerComponent').style.display = "none";
+    document.getElementById('progressBar').style.display = 'block';
+    if (!this.error || this.uploadFinished) {
+      this.resetInput();
+    }
+    this.clearFileError();
   }
 
+  // Open the form
   Open() {
     document.getElementById('close').style.display = 'block';
     document.getElementById('containerComponent').style.display = "block";
     document.getElementById('thankyou').style.display = 'none';
+    document.getElementById('progressBar').style.display = 'block';
   }
 }
